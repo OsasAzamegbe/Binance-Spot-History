7a61d11bd77f69c2a407f46555ca24c016cfb205
osamudiamen <osas.azamegbe@gmail.com>
2021-04-20 15:08:51 +0100

    generator to read from json file

--- businessUtils/fileIOUtils.py
+++ businessUtils/fileIOUtils.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union, Any
+from typing import Dict, List, Union, Any, Generator
 import pandas as pd
 import json
 
@@ -18,3 +18,12 @@ def write_to_excel(json_object: Union[List, Dict], filename: str) -> None:
     write a json object to an excel file (*.xlsx).
     '''
     pd.DataFrame(json_object).to_excel(f"{filename}_{str(timestamp())}.xlsx")
+
+
+def read_from_json(filename: str) -> Generator[Dict[str, Any], None, None]:
+    '''
+    generator that reads data from a json file and yields it as a dict,
+    '''
+    with open(filename, 'r') as json_file:
+        for json_object in json.load(json_file):
+            yield json_object
