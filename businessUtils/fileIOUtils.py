09a2616e6fb8c31bc8eff5cb51919abdeb37a6af
osamudiamen <osas.azamegbe@gmail.com>
2021-04-20 15:21:18 +0100

    refactor

--- businessUtils/fileIOUtils.py
+++ businessUtils/fileIOUtils.py
@@ -5,19 +5,25 @@ import json
 from businessUtils.apiUtils import timestamp
 
 
-def write_to_json(json_object: Union[List, Dict], filename: str) -> None:
+def write_to_json(json_object: Union[List, Dict], filename: str, replace_existing: bool=False) -> None:
     '''
     write a json object to a json file.
     '''
-    with open(f"{filename}_{str(timestamp())}.json", 'w') as file:
+    if not replace_existing:
+        filename = f"{filename}_{str(timestamp())}"
+
+    with open(f"{filename}.json", 'w') as file:
         json.dump(json_object, file, indent=4)
 
 
-def write_to_excel(json_object: Union[List, Dict], filename: str) -> None:
+def write_to_excel(json_object: Union[List, Dict], filename: str, replace_existing: bool=False) -> None:
     '''
     write a json object to an excel file (*.xlsx).
     '''
-    pd.DataFrame(json_object).to_excel(f"{filename}_{str(timestamp())}.xlsx")
+    if not replace_existing:
+        filename = f"{filename}_{str(timestamp())}"
+
+    pd.DataFrame(json_object).to_excel(f"{filename}.xlsx")
 
 
 def read_from_json(filename: str) -> Generator[Dict[str, Any], None, None]:
